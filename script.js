e6e86e27d372720870e8eef3b9bf6ca2716b7d7a
your_brain <144597205+Your-Brain@users.noreply.github.com>
2024-07-23 22:36:13 +0530

    update
    update

---
+++ script.js
@@ -0,0 +1,21 @@
+let x = ["hello", "I'm", "Partha", "I'm","a", "coder"];
+
+let typed1 = new Typed(".typing", {
+  strings: [...x],
+  typeSpeed: 150,
+  backSpeed: 15,
+  loop: true,
+  cursorChar: "",
+  smartBackspace: true,
+  showCursor: false,
+});
+let y = ["talitya", "jiakhali", "purba medinipur"];
+let typed2 = new Typed(".add", {
+  strings: [...y],
+  typeSpeed: 150,
+  backSpeed: 15,
+  loop: true,
+  cursorChar: "",
+  smartBackspace: true,
+  showCursor: false,
+});
